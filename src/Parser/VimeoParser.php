633f5fcb232df0e0c5e83b9dcb85e75451d25ea3
someApprentice <someApprentice@openmailbox.org>
2018-02-08 16:21:05 +0300

    Added check on Vimeo keys

--- src/Parser/VimeoParser.php
+++ src/Parser/VimeoParser.php
@@ -33,27 +33,31 @@ class VimeoParser implements ParserInterface
     {
         $result = new VimeoResult();
 
-        $vimeo = new Vimeo($this->options['client_id'], $this->options['client_secret']);
+        if (isset($this->options['client_id']) and isset($this->options['client_secret'])) {
+            $vimeo = new Vimeo($this->options['client_id'], $this->options['client_secret']);
 
-        $token = $vimeo->clientCredentials();
+            $token = $vimeo->clientCredentials();
 
-        $vimeo->setToken($token['body']['access_token']);
+            $vimeo->setToken($token['body']['access_token']);
 
-        $json = $vimeo->request("/videos/{$url->getId()}");
+            $json = $vimeo->request("/videos/{$url->getId()}");
 
-        if ($json) {
-            if (!isset($json['body']['error'])) {
-                $result->setTitle($json['body']['name']);
-                $result->setDescription($json['body']['description']);
-                $result->setPreview($json['body']['pictures']['sizes'][3]['link']);
-                $result->setEmbed($json['body']['embed']['html']);
-            } else {
-                $result->setErrors(['error' => 'Nothing found']);
+            if ($json) {
+                if (!isset($json['body']['error'])) {
+                    $result->setTitle($json['body']['name']);
+                    $result->setDescription($json['body']['description']);
+                    $result->setPreview($json['body']['pictures']['sizes'][3]['link']);
+                    $result->setEmbed($json['body']['embed']['html']);
+                } else {
+                    $result->setErrors(['error' => 'Nothing found']);
+                }
+
+                return $result;
             }
 
-            return $result;
+            throw new \Exception("Something went wrong in parsing stage");
         }
 
-        throw new \Exception("Something went wrong in parsing stage");
+        throw new \Exception("Define your API key in a config/hosting_options.ini file! See config/hosting_options.ini.example for example");
     }
 }
